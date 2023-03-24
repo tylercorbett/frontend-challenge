6f2fbfb96c12d3943734e63bda287eb88341e7ba
Tyler Corbett <thetylercorbett@gmail.com>
2023-03-24 11:15:12 -0700

    fallback img

--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -9,6 +9,8 @@ export interface StoryProps {
   id: string,
 }
 
+const fallbackAvatarSrc = 'https://chumley.barstoolsports.com/union/defaults/thumbnail.jpg?crop=4%3A3&width=560&height=560';
+
 const Story: React.FC<StoryProps> = ({ id, thumbnailSrc, title, authorAvatarSrc, authorName }) => {
   return (
     <li className={styles.Story} key={id}>
@@ -16,7 +18,7 @@ const Story: React.FC<StoryProps> = ({ id, thumbnailSrc, title, authorAvatarSrc,
       <div className={styles.body}>
         <h3 className={styles.title}>{title}</h3>
         <div className={styles.author}>
-          <img className={styles.avatar} src={authorAvatarSrc} alt={`Photo of ${authorName}`} />
+          <img className={styles.avatar} src={authorAvatarSrc ?? fallbackAvatarSrc} alt={`Photo of ${authorName}`} />
           <h4>{authorName}</h4>
         </div>
       </div>     
