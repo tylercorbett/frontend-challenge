36a3126bb773850c7515e6019580e10a770c1eac
Tyler Corbett <thetylercorbett@gmail.com>
2023-03-23 18:30:52 -0700

    most base grid in place

--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -8,10 +8,17 @@ interface Props {
   authorName: string,
 }
 
-const Story: React.FC = () => {
+const Story: React.FC<Props> = ({ thumbnailSrc = 'https://chumley.barstoolsports.com/union/defaults/thumbnail.jpg', title = 'Trust Me, You Want To Try Out This Warzone Season 4 Loadout', authorAvatarSrc = 'https://chumley.barstoolsports.com/union/defaults/thumbnail.jpg?crop=4%3A3', authorName = 'Skye Brewster' }) => {
   return (
     <li className={styles.Story}>
-        im a story
+      <img className={styles.thumbnail} src={thumbnailSrc} alt='Post thumbnail' /> 
+      <div>
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.author}>
+          <img className={styles.avatar} src={authorAvatarSrc} alt={`Photo of ${authorName}`} />
+          <h4>{authorName}</h4>
+        </div>
+      </div>     
     </li>
   );
 };
